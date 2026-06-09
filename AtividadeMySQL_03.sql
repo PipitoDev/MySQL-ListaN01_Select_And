dc27ccc5e4f722ab4f61a3e5a8b594c54ac272b4
PipitoDev <joaovitorsguizzato@gmail.com>
2026-06-09 10:28:41 -0300

    Fix SQL syntax for aliasing ProductID

---
+++ AtividadeMySQL_03.sql
@@ -0,0 +1,7 @@
+SELECT Quantity AS 'Ordem p/quantidade',
+	     ProductID AS 'ID do Produto',
+       OrderID AS 'ID da ordem'
+FROM OrderDetails
+WHERE ProductID >= 5 AND ProductID <= 15
+AND OrderID % 2 = 0
+ORDER BY Quantity ASC;
